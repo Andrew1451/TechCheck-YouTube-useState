e796dc41a4eab47bf2219879664151e681c32646
Andrew <andrew.gaynor@ymail.com>
2021-01-26 19:32:26 -0500

    teaching useState hook in App component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [showCount, setShowCount] = useState(true);
+  const [guitar, setGuitar] = useState({
+    brand: 'Fender',
+    model: 'Stratocaster'
+  })
+
+  // fetch(`api.com/${song}`).then(response => setSong({...song, artist: response.artist, song: response.song}))
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+      <button onClick={() => setShowCount(!showCount)}>toggle count</button>
+      <button onClick={() => setCount(count + 1)}>count it</button>
+      { showCount ? <p>{count}</p> : null }
+
+      <button onClick={() => setGuitar({...guitar, model: 'Telecaster'})}>change model</button>
+      <p>{guitar.brand}</p>
+      <p>{guitar.model}</p>
+
       </header>
     </div>
   );
